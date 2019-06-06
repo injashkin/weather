e50c0b01b38b0bd7a0c558a3b8922bbeefff27c0
injashkin <injashkin@gmail.com>
2019-06-06 23:05:31 +0300

    First commit

---
+++ src/WeatherDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class WeatherDisplay extends React.Component {
+  render() {
+    return (
+      <h1>Displaying some Weather!</h1>
+    );
+  }
+}
+
+export default WeatherDisplay;
