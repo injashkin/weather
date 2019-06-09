b858ce67a653cb4fbb11ea5f8a1855b0bed7b79a
injashkin <injashkin@gmail.com>
2019-06-09 23:39:45 +0300

    'asd'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import WeatherDisplay from './WeatherDisplay';
 
-function App() {  	
-	return (
-    	<div className="App">      
-      		<WeatherDisplay />
-    	</div>
-  	);
+const PLACES = [
+  { name: "Palo Alto", zip: "94303" },
+  { name: "San Jose", zip: "94088" },
+  { name: "Santa Cruz", zip: "95062" },
+  { name: "Honolulu", zip: "96803" }
+];
+
+class App extends React.Component {
+	constructor() {
+    	super();
+    	this.state = {
+      		activePlace: 0,
+    	};
+  	}  	
+	render() {
+		const activePlace = this.state.activePlace;
+		return (
+    		<div className="App">      			
+    			{PLACES.map((place, index) => (
+      				<button
+        				key={index}
+        				onClick={() => {
+          					this.setState({ activePlace: index });
+        				}}
+      				>
+          				{place.name}
+      				</button>
+    			))}
+    			<WeatherDisplay
+          			key={activePlace}
+          			zip={PLACES[activePlace].zip}
+        		/>
+    		</div>
+  		);
+    }
 }
 
 export default App;
